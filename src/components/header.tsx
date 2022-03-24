419b7666c90d6860b6846d4ce7ae41a93a31ae3c
Soujiro-a <qmaecx@naver.com>
2022-03-24 16:50:10 +0900

    Fixed Header Component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,15 +10,17 @@ export const Header: React.FC = () => {
   return (
     <>
       {!data?.me.verified && (
-        <div className="bg-red-500 py-3 px-3 text-center text-xs text-white">
+        <div className="bg-red-500 py-3 px-3 text-center text-base text-white">
           <span>이메일 인증을 완료해주세요</span>
         </div>
       )}
       <header className="py-4">
-        <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-          <img src={nuberLogo} alt={nuberLogo} className="w-24" />
+        <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
+          <Link to="/">
+            <img src={nuberLogo} alt={nuberLogo} className="w-36" />
+          </Link>
           <span className="text-xs">
-            <Link to="/my-profile">
+            <Link to="/edit-profile">
               <FontAwesomeIcon icon={faUser} className="text-xl" />
             </Link>
           </span>
