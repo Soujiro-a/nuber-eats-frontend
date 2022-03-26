2f60c3876996192d782e203ba079d913bbca3f63
Soujiro-a <qmaecx@naver.com>
2022-03-26 12:21:12 +0900

    Create Query and Categories Style

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,5 +1,82 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import {
+  restaurantsPageQuery,
+  restaurantsPageQueryVariables,
+} from "../../api/restaurantsPageQuery";
+
+const RESTAURANTS_QUERY = gql`
+  query restaurantsPageQuery($input: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImage
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImage
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
 
 export const Restaurants = () => {
-  return <h1>Restaurants</h1>;
+  const { data, loading } = useQuery<
+    restaurantsPageQuery,
+    restaurantsPageQueryVariables
+  >(RESTAURANTS_QUERY, {
+    variables: {
+      input: {
+        page: 1,
+      },
+    },
+  });
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          type="Search"
+          className="input rounded-md border-0 w-3/12"
+          placeholder="Search Restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories.categories?.map((category, index) => (
+              <div
+                className="flex flex-col items-center cursor-pointer"
+                key={index}
+              >
+                <div
+                  className="w-14 h-14 bg-cover hover:bg-gray-100 rounded-full"
+                  style={{ backgroundImage: `url(${category.coverImage})` }}
+                ></div>
+                <span className="mt-1 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
