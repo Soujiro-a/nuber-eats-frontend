33e840aad18cce3598f86915fa32f64a705dfe72
Soujiro-a <qmaecx@naver.com>
2022-03-27 12:37:50 +0900

    Create Restaurants Pagenation logic

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,5 +1,122 @@
-import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import {
+  restaurantsPageQuery,
+  restaurantsPageQueryVariables,
+} from "../../api/restaurantsPageQuery";
+import { Restaurant } from "../../components/restaurant";
+
+const RESTAURANTS_QUERY = gql`
+  query restaurantsPageQuery($input: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImage
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImage
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
 
 export const Restaurants = () => {
-  return <h1>Restaurants</h1>;
+  const [page, setPage] = useState(1);
+  const { data, loading } = useQuery<
+    restaurantsPageQuery,
+    restaurantsPageQueryVariables
+  >(RESTAURANTS_QUERY, {
+    variables: {
+      input: {
+        page,
+      },
+    },
+  });
+  const onPrevPageClick = () => setPage((current) => current - 1);
+  const onNextPageClick = () => setPage((current) => current + 1);
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          type="Search"
+          className="input rounded-md border-0 w-3/12"
+          placeholder="Search Restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories.categories?.map((category, index) => (
+              <div
+                className="flex flex-col group items-center cursor-pointer"
+                key={index}
+              >
+                <div
+                  className="w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
+                  style={{ backgroundImage: `url(${category.coverImage})` }}
+                ></div>
+                <span className="mt-1 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className="grid mt-16 grid-cols-3 gap-x-5 gap-y-10">
+            {data?.restaurants.results?.map((restaurant, index) => (
+              <Restaurant
+                id={restaurant.id + ""}
+                coverImage={restaurant.coverImage}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+                key={index}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="font-medium text-2xl focus:outline-none"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {data?.restaurants.totalPages}
+            </span>
+            {page !== data?.restaurants.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="font-medium text-2xl focus:outline-none"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
