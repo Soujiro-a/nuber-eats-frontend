bd4eaf73800ff6da8e3bdc19774e93a90fb3c9c0
Soujiro-a <qmaecx@naver.com>
2022-03-27 12:37:25 +0900

    Create Restaurant List Component

---
+++ src/components/restaurant.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface IRestaurantProps {
+  id: string;
+  coverImage: string;
+  name: string;
+  categoryName?: string;
+}
+
+export const Restaurant: React.FC<IRestaurantProps> = ({
+  id,
+  coverImage,
+  name,
+  categoryName,
+}) => (
+  <div className="flex flex-col">
+    <div
+      className="bg-cover py-28 bg-center mb-3"
+      style={{ backgroundImage: `url(${coverImage})` }}
+    ></div>
+    <h3 className="text-xl font-medium">{name}</h3>
+    <span className="border-t mt-3 py-3 text-xs opacity-50 border-gray-400">
+      {categoryName}
+    </span>
+  </div>
+);
