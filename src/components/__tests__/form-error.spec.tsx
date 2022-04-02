b964a683bcc192093b0a4ba94fcd8909d477b3a5
Soujiro-a <qmaecx@naver.com>
2022-04-02 09:13:12 +0900

    Create form-error test file

---
+++ src/components/__tests__/form-error.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { FormError } from "../form-error";
+
+describe("FormError", () => {
+  it("에러 타입의 폼이 렌더링됩니다.", () => {
+    render(<FormError errorMessage="test" />);
+    screen.getByText("test");
+  });
+});
