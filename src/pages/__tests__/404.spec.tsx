ab62d70c52d19e3626fbe325b64a98652f56d6b5
Soujiro-a <qmaecx@naver.com>
2022-04-02 10:01:41 +0900

    Create page test folder, and Create 404 page test file

---
+++ src/pages/__tests__/404.spec.tsx
@@ -0,0 +1,20 @@
+import { render, waitFor } from "@testing-library/react";
+import React from "react";
+import { HelmetProvider } from "react-helmet-async";
+import { NotFound } from "../404";
+import { BrowserRouter as Router } from "react-router-dom";
+
+describe("404 NotFound", () => {
+  it("404 NotFound 페이지를 렌더링합니다.", async () => {
+    render(
+      <HelmetProvider>
+        <Router>
+          <NotFound />
+        </Router>
+      </HelmetProvider>
+    );
+    await waitFor(async () => {
+      expect(document.title).toBe("Not Found | Nuber Eats");
+    });
+  });
+});
