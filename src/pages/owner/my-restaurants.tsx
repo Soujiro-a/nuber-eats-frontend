34ce8c4dbe8b2209bacb9e3906f2a7e0e778b8a7
Soujiro-a <qmaecx@naver.com>
2022-04-19 10:18:11 +0900

    Fixed myRestaurant page in ownerRoute

--- src/pages/owner/my-restaurants.tsx
+++ src/pages/owner/my-restaurants.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import { myRestaurants } from "../../api/myRestaurants";
+import { Restaurant } from "../../components/restaurant";
 import { RESTAURANT_FRAGMENT } from "../../fragments";
 
-const MY_RESTAURANTS_QUERY = gql`
+export const MY_RESTAURANTS_QUERY = gql`
   query myRestaurants {
     myRestaurants {
       ok
@@ -28,13 +29,26 @@ export const MyRestaurants = () => {
       <div className="container mt-32">
         <h2 className="text-4xl font-medium mb-10">My Restaurants</h2>
 
-        {data?.myRestaurants.ok && data.myRestaurants.restaurants.length === 0 && (
+        {data?.myRestaurants.ok &&
+        data.myRestaurants.restaurants.length === 0 ? (
           <>
             <h4 className="text-xl mb-5">등록된 음식점이 없습니다.</h4>
             <Link className="link" to="/add-restaurant">
               음식점을 등록하세요! &rarr;
             </Link>
           </>
+        ) : (
+          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+            {data?.myRestaurants.restaurants.map((restaurant, index) => (
+              <Restaurant
+                id={restaurant.id + ""}
+                coverImage={restaurant.coverImage}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+                key={index}
+              />
+            ))}
+          </div>
         )}
       </div>
     </div>
