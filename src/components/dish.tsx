d137043572512ae68061a20f55d27c85631bc038
Soujiro-a <qmaecx@naver.com>
2022-05-02 19:24:51 +0900

    Fixed dish Component

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -1,17 +1,70 @@
+import { restaurantPageQuery_restaurant_restaurant_menu_options } from "../api/restaurantPageQuery";
+
 interface IDishProps {
+  id?: number;
   description: string;
   name: string;
   price: number;
+  isCustomer?: boolean;
+  options?: restaurantPageQuery_restaurant_restaurant_menu_options[] | null;
+  orderStarted?: boolean;
+  addItemToOrder?: (dishId: number) => void;
+  removeFromOrder?: (dishId: number) => void;
+  isSelected?: boolean;
 }
 
-export const Dish: React.FC<IDishProps> = ({ description, name, price }) => {
+export const Dish: React.FC<IDishProps> = ({
+  id = 0,
+  description,
+  name,
+  price,
+  isCustomer = false,
+  options,
+  orderStarted = false,
+  addItemToOrder,
+  removeFromOrder,
+  isSelected,
+  children: dishOptions,
+}) => {
+  const onClick = () => {
+    if (orderStarted) {
+      if (!isSelected && addItemToOrder) {
+        return addItemToOrder(id);
+      }
+      if (isSelected && removeFromOrder) {
+        return removeFromOrder(id);
+      }
+    }
+  };
   return (
-    <div className="px-8 py-4 border hover:border-gray-800 transition-all">
+    <div
+      className={`px-8 py-4 border cursor-pointer hover:border-gray-800 transition-all ${
+        isSelected ? "border-gray-800" : "hover:border-gray-800"
+      }`}
+    >
       <div className="mb-5">
-        <h3 className="text-lg font-medium">{name}</h3>
+        <h3 className="text-lg font-medium">
+          {name}{" "}
+          {orderStarted && (
+            <button
+              className={`btn ml-2 p-1 ${
+                isSelected ? "bg-red-600" : "bg-lime-600"
+              }`}
+              onClick={onClick}
+            >
+              {isSelected ? "취소" : "선택"}
+            </button>
+          )}
+        </h3>
         <h4 className="font-medium">{description}</h4>
       </div>
       <span>&#x20a9;{price}</span>
+      {isCustomer && options && options?.length !== 0 && (
+        <div>
+          <h5 className="mt-8 mb-3 font-bold">추가 구성</h5>
+          {dishOptions}
+        </div>
+      )}
     </div>
   );
 };
