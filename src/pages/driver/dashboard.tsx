c5b2716a0f0d5c656a188046eb3a8649d136322a
Soujiro-a <qmaecx@naver.com>
2022-05-17 17:29:35 +0900

    Fixed Dashboard page for driver

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,15 +1,46 @@
 import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 import { Helmet } from "react-helmet-async";
+import { gql, useMutation, useSubscription } from "@apollo/client";
+import { FULL_ORDER_FRAGMENT } from "../../fragments";
+import { cookedOrders } from "../../api/cookedOrders";
+import { useNavigate } from "react-router-dom";
+import { takeOrder, takeOrderVariables } from "../../api/takeOrder";
+
+const COOKED_ORDERS_SUBSCRIPTION = gql`
+  subscription cookedOrders {
+    cookedOrders {
+      ...FullOrderParts
+    }
+  }
+  ${FULL_ORDER_FRAGMENT}
+`;
+
+const TAKE_ORDER_MUTATION = gql`
+  mutation takeOrder($input: TakeOrderInput!) {
+    takeOrder(input: $input) {
+      ok
+      error
+    }
+  }
+`;
 
 interface ICoords {
   lat: number;
   lng: number;
 }
 
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🛵</div>;
+
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
-  const [map, setMap] = useState<any>();
+  const [map, setMap] = useState<google.maps.Map>();
   const [maps, setMaps] = useState<any>();
   const onSuccess = ({
     coords: { latitude, longitude },
@@ -26,13 +57,81 @@ export const Dashboard = () => {
   }, []);
   useEffect(() => {
     if (map && maps) {
-      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode(
+        {
+          location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+        },
+        (results, status) => {
+          console.log(status, results);
+        }
+      );
     }
   }, [driverCoords.lat, driverCoords.lng, map, maps]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
     setMap(map);
     setMaps(maps);
+  };
+  const makeRoute = () => {
+    if (map) {
+      const directionService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer();
+      directionsRenderer.setMap(map);
+      directionService.route(
+        {
+          origin: {
+            location: new google.maps.LatLng(
+              driverCoords.lat,
+              driverCoords.lng
+            ),
+          },
+          destination: {
+            location: new google.maps.LatLng(
+              driverCoords.lat + 0.1,
+              driverCoords.lng + 0.1
+            ),
+          },
+          travelMode: google.maps.TravelMode.TRANSIT,
+        },
+        (result) => {
+          directionsRenderer.setDirections(result);
+        }
+      );
+    }
+  };
+  const { data: cookedOrdersData } = useSubscription<cookedOrders>(
+    COOKED_ORDERS_SUBSCRIPTION
+  );
+  useEffect(() => {
+    if (cookedOrdersData?.cookedOrders.id) {
+      makeRoute();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cookedOrdersData]);
+  const navigate = useNavigate();
+  const onCompleted = (data: takeOrder) => {
+    if (data.takeOrder.ok) {
+      navigate(`orders/${cookedOrdersData?.cookedOrders.id}`, {
+        replace: true,
+      });
+    }
+  };
+  const [takeOrderMutation] = useMutation<takeOrder, takeOrderVariables>(
+    TAKE_ORDER_MUTATION,
+    {
+      onCompleted,
+    }
+  );
+  const triggerMutation = (orderId: number) => {
+    takeOrderMutation({
+      variables: {
+        input: {
+          id: orderId,
+        },
+      },
+    });
   };
   return (
     <div>
@@ -53,15 +152,30 @@ export const Dashboard = () => {
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
         >
-          <div
-            //@ts-ignore
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-            className="text-lg"
-          >
-            🛵
-          </div>
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
         </GoogleMapReact>
+      </div>
+
+      <div className="max-w-screen-sm mx-auto bg-white relative -top-10 shadow-lg py-8 px-5">
+        {cookedOrdersData?.cookedOrders.restaurant ? (
+          <>
+            <h1 className="text-center text-3xl font-medium">새 주문입니다!</h1>
+            <h1 className="text-center my-3 font-medium">
+              @{cookedOrdersData.cookedOrders.restaurant?.name} 매장에 픽업하러
+              가세요!
+            </h1>
+            <button
+              onClick={() => triggerMutation(cookedOrdersData?.cookedOrders.id)}
+              className="btn w-full block text-center mt-5"
+            >
+              배달 받기 &rarr;
+            </button>
+          </>
+        ) : (
+          <h1 className="text-center text-3xl font-medium">
+            아직 주문이 없습니다.
+          </h1>
+        )}
       </div>
     </div>
   );
